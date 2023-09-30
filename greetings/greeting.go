8a33f39cf5a25928ec4d3ffa1cbdcf54fde5fe4b
Xuemin Guan <xueminguan@xuemins-mbp.lan>
2023-09-30 20:29:03 +0100

    XG: random greeting

---
+++ greetings/greeting.go
@@ -0,0 +1,29 @@
+package greetings
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
+
+// Hello returns a greeting for the named person.
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// If a name was received, return a value that embeds the name
+	// in a greeting message.
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+func randomFormat() string {
+	greetings := []string{
+		"Hello, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+	return greetings[rand.Intn(len(greetings))]
+}
